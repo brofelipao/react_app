fa2449da7c9dcbb2cb8ad16d6198afc30aa7fec6
lmonteiro <lmonteiro@grupoimagetech.com.br>
2024-01-22 16:48:09 -0400

    tela index

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,12 +1,39 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { 
+  View,
+  Text,
+  TextInput, 
+  TouchableOpacity,
+  Image, 
+  FlatList // dropdown list
+} from 'react-native'
+import { useState } from 'react'
+import { useRouter } from 'expo-router'
 
 import styles from './welcome.style'
+import { icons, SIZES  } from '../../../constants'
 
 const Welcome = () => {
+  const router = useRouter();
+
   return (
     <View>
-      <Text>Welcome</Text>
+      <View style={styles.container}>
+        <Text style={styles.userName}>Olá, Luiz!</Text>
+        <Text style={styles.welcomeMessage}>Encontre uma vaga de emprego.</Text>
+      </View>
+
+      <View style={styles.searchContainer} >
+        <View style={styles.searchWrapper} >
+          <TextInput 
+            style={styles.searchInput} 
+            value=''
+            onChange={() => {
+
+            }}
+            placeholder='O que você está procurando?'/>
+        </View>
+      </View>
     </View>
   )
 }
